2d4b2e52a6e1642e994fc2cd936d32115b48e5e2
emile <emilepicard323@gmail.com>
2024-12-05 16:45:47 +0100

    finish

--- app/models/produits.py
+++ app/models/produits.py
@@ -7,3 +7,5 @@ class Produit(models.Model):
     prix = models.DecimalField(max_digits=10, decimal_places=2)
     quantite = models.IntegerField()
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    date_peremtion = models.DateTimeField(null=True, blank=True)
+    emplacement = models.CharField(max_length=100)
